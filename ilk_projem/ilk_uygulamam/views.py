723a9ff7f8e31cc1e9abe6ff676e852c258564f8
Deveb <djangoogreniyorum@gmail.com>
2023-10-26 01:34:52 +0300

    index() işlevi oluşturduk.
    Bir önceki adımlarda views.py'yi tetikleyerek HttpResponse() işlevini işletebileceğimizi ancak birden çok ilgisiz tetikleme ile yeniden işleyeceğini dillendirmiştik. Bunun sıkıntı çıkaracağını söylemiştik. Biz de index() adında bir değişken oluşturup HttpResonse() işlevini kuşattık. Böylece biz index() işlevini bir yerde tetiklemedikçe bu işlemler işlemeyecektir.

--- ilk_projem/ilk_uygulamam/views.py
+++ ilk_projem/ilk_uygulamam/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-HttpResponse("Merhaba, Dünya. Bu benim ilk projem.")
+def index(request):
+    HttpResponse("Merhaba, Dünya. Bu benim ilk projem.")
